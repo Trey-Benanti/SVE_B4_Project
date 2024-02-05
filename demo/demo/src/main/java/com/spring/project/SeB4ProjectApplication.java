1ef978ecadf4537d656a42a8bc5b6a66e01f8382
Trey-Benanti <119818212+Trey-Benanti@users.noreply.github.com>
2024-02-05 18:02:47 -0500

    Add files via upload

---
+++ demo/demo/src/main/java/com/spring/project/SeB4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeB4ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeB4ProjectApplication.class, args);
+	}
+
+}
