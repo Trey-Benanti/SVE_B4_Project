6f1f3e3013ee88ef103f23ddbf59400a9a212503
Scott Wagner <79953791+ScottWagner100@users.noreply.github.com>
2024-02-17 20:29:02 -0500

    Movie.java model created + getters/setters

---
+++ demo/demo/src/main/java/com/spring/project/models/Movie.java
@@ -0,0 +1,102 @@
+package com.spring.project.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String movieTitle;
+    private String cast;
+    private String director;
+    private String producer;
+    private String synop;
+    private String reviews;
+    private String trailerPhoto;
+    private String trailerVideo;
+    private String rating;
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getSynop() {
+        return synop;
+    }
+
+    public void setSynop(String synop) {
+        this.synop = synop;
+    }
+
+    public String getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(String reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getTrailerPhoto() {
+        return trailerPhoto;
+    }
+
+    public void setTrailerPhoto(String trailerPhoto) {
+        this.trailerPhoto = trailerPhoto;
+    }
+
+    public String getTrailerVideo() {
+        return trailerVideo;
+    }
+
+    public void setTrailerVideo(String trailerVideo) {
+        this.trailerVideo = trailerVideo;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+} // Movie
