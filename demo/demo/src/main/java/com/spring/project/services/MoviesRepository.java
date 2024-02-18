8b29ea00da2619a21574907446f402e8200c3714
Scott Wagner <79953791+ScottWagner100@users.noreply.github.com>
2024-02-18 00:11:14 -0500

    Migrated to local DB

--- demo/demo/src/main/java/com/spring/project/services/MoviesRepository.java
+++ demo/demo/src/main/java/com/spring/project/services/MoviesRepository.java
@@ -3,9 +3,4 @@ package com.spring.project.services;
 import com.spring.project.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MoviesRepository extends JpaRepository<Movie, Integer> {
-
-
-
-
-} // MoviesRepository
+public interface MoviesRepository extends JpaRepository<Movie, Integer> { } // MoviesRepository
