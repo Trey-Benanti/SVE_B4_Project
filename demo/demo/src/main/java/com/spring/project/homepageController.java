0e64437355033bc716090bdab3de6fd7b02decb6
Scott Wagner <79953791+ScottWagner100@users.noreply.github.com>
2024-02-18 03:45:12 -0500

    Added feature: add movie to DB, appears on homepage

--- demo/demo/src/main/java/com/spring/project/homepageController.java
+++ demo/demo/src/main/java/com/spring/project/homepageController.java
@@ -5,8 +5,13 @@ import com.spring.project.models.MovieDTO;
 import com.spring.project.services.MoviesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 @Controller
@@ -31,6 +36,30 @@ public class homepageController {
         model.addAttribute("movieDTO", movieDTO);
         return "create";
     } // create
+
+    @PostMapping("/create")
+    public String createMovie(
+            @Valid @ModelAttribute MovieDTO movieDTO,
+            BindingResult result
+    ) {
+        if (result.hasErrors()) {return "/create";} // Return to create if form input error
+
+        // Post form input to DB
+        Movie movie = new Movie();
+        movie.setMovieTitle(movieDTO.getMovieTitle());
+        movie.setCast(movieDTO.getCast());
+        movie.setDirector(movieDTO.getDirector());
+        movie.setProducer(movieDTO.getProducer());
+        movie.setRating(movieDTO.getRating());
+        movie.setReviews(movieDTO.getReviews());
+        movie.setSynop(movieDTO.getSynop());
+        movie.setTrailerPhoto(movie.getTrailerPhoto());
+        movie.setTrailerVideo(movieDTO.getTrailerVideo());
+
+        repo.save(movie);
+
+        return "redirect:/"; // redirect home
+    }
 
     @GetMapping("/adminview")
     public String adminView() {
