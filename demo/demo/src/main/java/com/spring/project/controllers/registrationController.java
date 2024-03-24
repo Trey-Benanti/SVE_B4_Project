0ecbe6ae6aea29065e777b629b5b203f3437c24c
Sam Wilson <saw31815@uga.edu>
2024-03-24 12:23:51 -0400

    Send Email

--- demo/demo/src/main/java/com/spring/project/controllers/registrationController.java
+++ demo/demo/src/main/java/com/spring/project/controllers/registrationController.java
@@ -1,6 +1,10 @@
 package com.spring.project.controllers;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.spring.project.users.*;
 import com.spring.project.services.UserRepository;
 
+import java.io.UnsupportedEncodingException;
+
 @Controller
 public class registrationController {
 
     @Autowired
     private UserRepository userRepo; // Reference to user repository interface
 
+    @Autowired
+    private JavaMailSender mailSender;
 
     @GetMapping("/confirm")
     public String confirm() { 
@@ -29,22 +37,48 @@ public class registrationController {
     } // signup
 
     @PostMapping("/process_register")
-    public String processRegister(User user) {
+    public String processRegister(User user) throws MessagingException, UnsupportedEncodingException {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
 
         // checking if email exists
-        User existingAccount = userRepo.findByEmail(user.getEmail());
+        // TODO: route back to registration page
+ /*       User existingAccount = userRepo.findByEmail(user.getEmail());
         if (existingAccount != null)
         {
             return "account_exists";
         }
 
         // TODO: send verification email and save code (spring-starter-mail)
+       // sending verification code
+        EmailDetails ver = new EmailDetails();
+        ver.setRecipient(user.getEmail());
+        ver.setSubject("test");
+        ver.setMsgBody("test");
+
+        EmailService sender = new EmailService();
+        sender.sendEmail(ver);
+*/
+        sendVerEmail(user);
 
         userRepo.save(user);
     
         return "register_success";
     }
+
+    public void sendVerEmail(User user) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("samuelwilson1123@gmail.com", "test");
+        helper.setTo(user.getEmail());
+        helper.setSubject("testSub");
+        helper.setText("test text");
+
+        System.out.println("SENDING to " + user.getEmail());
+        mailSender.send(message);
+
+    }
+
 }
