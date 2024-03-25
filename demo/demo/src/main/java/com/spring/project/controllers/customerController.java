1a54b137c2b7c39492be592c3289951b0c9de982
Juan Ochoa <jmo44123@uga.edu>
2024-03-25 12:27:00 -0400

    Update customerController.java

--- demo/demo/src/main/java/com/spring/project/controllers/customerController.java
+++ demo/demo/src/main/java/com/spring/project/controllers/customerController.java
@@ -10,6 +10,25 @@ public class customerController {
     public String profile() {
         return "profile";
     } // profile
+    
+    @GetMapping("/editprofile")
+    public String editprofile() {
+        return "editprofile";
+    } // editprofile
+    
+    @GetMapping("/creditcards")
+    public String creditcards() {
+        return "creditcards";
+    } // creditcards
 
+    @GetMapping("/editcards")
+    public String editcards() {
+        return "editcards";
+    } // editcards
+
+    @GetMapping("/orders")
+    public String orders() {
+        return "orders";
+    } // orders
 } // homepageController
 
