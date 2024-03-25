c49aafd47639923e7ef11f723c424aea29c091e8
Juan Ochoa <jmo44123@uga.edu>
2024-03-25 18:06:56 -0400

    Updated Admin css

--- demo/demo/src/main/java/com/spring/project/controllers/customerController.java
+++ demo/demo/src/main/java/com/spring/project/controllers/customerController.java
@@ -11,5 +11,24 @@ public class customerController {
         return "profile";
     } // profile
 
+    @GetMapping("/editprofile")
+    public String editprofile() {
+        return "editprofile";
+    } // editprofile
+
+    @GetMapping("/creditcards")
+    public String creditcards() {
+        return "creditcards";
+    } // creditcards
+
+    @GetMapping("/editcards")
+    public String editcards() {
+        return "editcards";
+    } // editcards
+
+    @GetMapping("/orders")
+    public String orders() {
+        return "orders";
+    } // orders
 } // homepageController
 
