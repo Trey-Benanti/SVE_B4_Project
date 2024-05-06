bdb547824e9809807b50940473f5009d0220616d
Scott Wagner <79953791+ScottWagner100@users.noreply.github.com>
2024-05-06 01:18:13 -0400

    Cards can be selected during checkout.

--- demo/demo/src/main/java/com/spring/project/controllers/bookingController.java
+++ demo/demo/src/main/java/com/spring/project/controllers/bookingController.java
@@ -1,5 +1,6 @@
 package com.spring.project.controllers;
 
+import java.security.Principal;
 import java.util.List;
 
 import com.spring.project.models.movies.Movie;
@@ -8,11 +9,18 @@ import com.spring.project.models.movies.movieservices.MoviesRepository;
 import com.spring.project.models.shows.Show;
 import com.spring.project.models.shows.showservices.ShowRepository;
 
+import com.spring.project.models.users.User;
+import com.spring.project.models.users.userinfo.CardInfo;
+import com.spring.project.models.users.userservices.CardInfoRepository;
+import com.spring.project.models.users.userservices.UserRepository;
+import com.spring.project.services.EncryptFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class bookingController {
@@ -24,6 +32,14 @@ public class bookingController {
 
     @Autowired
     private MoviesRepository repo; // Reference to movie repository interface
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private CardInfoRepository cardRepo;
+
+    private EncryptFacade encrypt = EncryptFacade.getInstance();
 
     @GetMapping("/select-show")
     public String selectShow() {
@@ -56,8 +72,24 @@ public class bookingController {
         return "selectshow";
     }
     @GetMapping("/checkout")
-    public String checkout() {
+    public String checkout(CardInfo cardInfo, Principal principal, Model model) throws Exception {
+        if(principal == null) {return "homepage";}
+
+        User user = userRepo.findByEmail(principal.getName());
+        List<CardInfo> cards = user.getPaymentInfo();
+
+        for(int i = 0; i < cards.size(); i++) {
+            if(!cards.get(i).getCardNumber().equals("")) {
+
+                String decCardNum = encrypt.decryptCard(cards.get(i));
+                String cardLastFour = decCardNum.substring(decCardNum.length() - 4);
+                cardLastFour = "**** **** **** " + cardLastFour;
+                cards.get(i).setCardNumber(cardLastFour);
+            } // if
+        } // for
+        model.addAttribute("cards", cards);
         return "checkout";
     } // checkout
+
 
 }
