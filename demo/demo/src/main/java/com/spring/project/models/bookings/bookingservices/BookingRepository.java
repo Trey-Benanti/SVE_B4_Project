26910ccc8aca86228c50b13cc8fdf398ce2e77dc
Sam Wilson <saw31815@uga.edu>
2024-05-06 16:30:06 -0400

    find by booking by user

--- demo/demo/src/main/java/com/spring/project/models/bookings/bookingservices/BookingRepository.java
+++ demo/demo/src/main/java/com/spring/project/models/bookings/bookingservices/BookingRepository.java
@@ -2,8 +2,15 @@ package com.spring.project.models.bookings.bookingservices;
 
 import com.spring.project.models.bookings.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+    @Query(value = "SELECT * FROM bookings WHERE user_id = (?!)", nativeQuery = true)
+    List<Booking> findBookingByUser(int user_id);
+
 }
